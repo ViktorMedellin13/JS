572ad405e9a15e38fb16511b1f44c76777b0f4c2
Viktor <rulya13@mail.ru>
2022-04-05 14:25:44 +0300

    добавлен свич

---
+++ task2/switch/js/switch.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let sum = 1000; 
+let code = prompt("code for discount");
+switch (code) {
+    case "4653":
+        alert("discount - 30%");
+        break;
+    case  "5698":
+    case  "5111":
+        alert("discount - 20%");
+        break;
+    case  "6922":
+    case  "6113":
+    case  "6099"
+        alert("discount - 10%");
+        break;
+    default:
+    alert()
+}  // ошибка
